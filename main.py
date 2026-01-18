d8579d4703c5c8e972a4c0502582aa424ea56c87
zx3005511-dev <zx3005511@gmail.com>
2026-01-18 22:21:16 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN").strip()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("البوت شغّال بنجاح 🤖🔥")
