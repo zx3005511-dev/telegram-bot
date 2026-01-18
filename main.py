b37871e1788759604bf0e9dc99ae680ecde9a311
zx3005511-dev <zx3005511@gmail.com>
2026-01-19 00:47:53 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,16 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = os.getenv("BOT_TOKEN")
 
 if not TOKEN:
-    raise ValueError("BOT_TOKEN is missing")
+    raise RuntimeError("BOT_TOKEN not found in environment variables")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("🤖🔥 البوت شغال بنجاح!")
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Bot started successfully...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
